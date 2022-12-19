99efd9be07f427a0e13cdba07d4415e3b553f9be
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-19 14:38:30 +0530

    improving solution for day 18 part 1

--- Day 18/solution_p1.py
+++ Day 18/solution_p1.py
@@ -3,18 +3,22 @@ import time
 
 with open('input.txt') as f:
     start = time.time()
-    coordinates = []
+    coordinates = set()
+    offsets = [(0, 0, 1),
+               (0, 0, -1),
+               (0, 1, 0),
+               (0, -1, 0),
+               (1, 0, 0),
+               (-1, 0, 0)]
     for line in f.read().split('\n'):
         x, y, z = list(map(int, line.split(',')))
-        coordinates.append((x, y, z))
+        coordinates.add((x, y, z))
     total_edges = len(coordinates) * 6
-    for i in range(len(coordinates)):
-        for j in range(len(coordinates)):
-            if i != j:
-                x1, y1, z1 = coordinates[i]
-                x2, y2, z2 = coordinates[j]
-                adjacency = abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2)
-                if adjacency == 1:
-                    total_edges -= 1
+    for coordinate in list(coordinates):
+        x1, y1, z1 = coordinate
+        for offset in offsets:
+            x2, y2, z2 = offset
+            if (x1 + x2, y1 + y2, z1 + z2) in coordinates:
+                total_edges -= 1
     print('Number of exposed edges :', total_edges)
     print('Time taken :', time.time() - start)

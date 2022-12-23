da1db612a89eaa2244955f7619a0aeebb41e18c1
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-23 14:10:30 +0530

    adding solution for Day 21 part 2

---
+++ Day 21/solution_p2.py
@@ -0,0 +1,34 @@
+import time
+from sympy.solvers import solve
+
+
+def parse(line):
+    line = line.split(': ')
+    monkey = line[0]
+    if str.isnumeric(line[1]):
+        return monkey, int(line[1])
+    else:
+        return monkey, line[1].split(' ')
+
+
+def form_equation(monkey, graph, equation):
+    curr = graph[monkey]
+    if monkey == 'humn':
+        return 'x'
+    elif isinstance(curr, (int, float)):
+        return graph[monkey]
+    return '(' + str(form_equation(curr[0], graph, equation)) + curr[1] + str(form_equation(curr[2], graph, equation)) + ')'
+
+
+with open('input.txt') as f:
+    start = time.time()
+    graph = dict()
+    lines = f.read().split('\n')
+    graph = dict()
+    for line in lines:
+        monkey, job = parse(line)
+        graph[monkey] = job
+    graph['root'] = [graph['root'][0], '-', graph['root'][2]]
+    equation = form_equation('root', graph, str())
+    print('The number you yell is :', solve(equation, 'x')[0])
+    print('Time taken :', time.time() - start)

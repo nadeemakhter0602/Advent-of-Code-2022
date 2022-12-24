8902d3a0c803b85e88f90b2f9a7fb4464444b2ee
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2022-12-24 17:29:08 +0530

    day 22 part 1, modularizing and cleaning

--- Day 22/solution_p1.py
+++ Day 22/solution_p1.py
@@ -2,12 +2,122 @@ import re
 import time
 
 
+def move(steps, facing, row, col, board):
+    if facing == 'R':
+        return move_right(steps, row, col, board)
+    elif facing == 'L':
+        return move_left(steps, row, col, board)
+    elif facing == 'U':
+        return move_up(steps, row, col, board)
+    elif facing == 'D':
+        return move_down(steps, row, col, board)
+
+
+def move_right(steps, row, col, board):
+    col_start = 0
+    for i in range(len(board[row])):
+        if board[row][i] != ' ':
+            col_start = i
+            break
+    col_end = len(board[row]) - 1
+    while steps > 0:
+        if board[row][col] == '.':
+            steps -= 1
+            col += 1
+        if col > col_end:
+            col = col_start
+        if board[row][col] == '#':
+            col -= 1
+            if col < col_start:
+                col = col_end
+            return row, col
+    return row, col
+
+
+def move_left(steps, row, col, board):
+    col_start = 0
+    for i in range(len(board[row])):
+        if board[row][i] != ' ':
+            col_start = i
+            break
+    col_end = len(board[row]) - 1
+    while steps > 0:
+        if board[row][col] == '.':
+            steps -= 1
+            col -= 1
+        if col < col_start:
+            col = col_end
+        if board[row][col] == '#':
+            col += 1
+            if col > col_end:
+                col = col_start
+            return row, col
+    return row, col
+
+
+def move_up(steps, row, col, board):
+    row_start = 0
+    for i in range(len(board)):
+        if len(board[i]) - 1 >= col and board[i][col] != ' ':
+            row_start = i
+            break
+    row_end = 0
+    for i in range(row_start, len(board)):
+        if len(board[i]) - 1 >= col and board[i][col] != ' ':
+            row_end = i
+    while steps > 0:
+        if board[row][col] == '.':
+            steps -= 1
+            row -= 1
+        if row < row_start:
+            row = row_end
+        if board[row][col] == '#':
+            row += 1
+            if row > row_end:
+                row = row_start
+            return row, col
+    return row, col
+
+
+def move_down(steps, row, col, board):
+    row_start = 0
+    for i in range(len(board)):
+        if len(board[i]) - 1 >= col and board[i][col] != ' ':
+            row_start = i
+            break
+    row_end = 0
+    for i in range(row_start, len(board)):
+        if len(board[i]) - 1 >= col and board[i][col] != ' ':
+            row_end = i
+    while steps > 0:
+        if board[row][col] == '.':
+            steps -= 1
+            row += 1
+        if row > row_end:
+            row = row_start
+        if board[row][col] == '#':
+            row -= 1
+            if row < row_start:
+                row = row_end
+            return row, col
+    return row, col
+
+
 with open('input.txt') as f:
     start = time.time()
     parts = f.read().split('\n\n')
     board = [i for i in parts[0].split('\n')]
     directions = re.findall(r'\d+|L|R', parts[1].strip())
     directions = [int(i) if i.isnumeric() else i for i in directions]
+    direction_facing = dict()
+    direction_facing['R'] = {'L': 'U',
+                             'R': 'D',
+                             'U': 'R',
+                             'D': 'L'}
+    direction_facing['L'] = {'L': 'D',
+                             'R': 'U',
+                             'U': 'L',
+                             'D': 'R'}
     row = 0
     col = 0
     for i in range(len(board[row])):
@@ -16,103 +126,10 @@ with open('input.txt') as f:
             break
     facing = 'R'
     for direction in directions:
-        if direction == 'R':
-            if facing == 'L':
-                facing = 'U'
-            elif facing == 'R':
-                facing = 'D'
-            elif facing == 'U':
-                facing = 'R'
-            elif facing == 'D':
-                facing = 'L'
-        elif direction == 'L':
-            if facing == 'L':
-                facing = 'D'
-            elif facing == 'R':
-                facing = 'U'
-            elif facing == 'U':
-                facing = 'L'
-            elif facing == 'D':
-                facing = 'R'
-        elif isinstance(direction, int):
-            if facing == 'R':
-                col_start = 0
-                for i in range(len(board[row])):
-                    if board[row][i] != ' ':
-                        col_start = i
-                        break
-                col_end = len(board[row]) - 1
-                while direction > 0:
-                    if board[row][col] == '.':
-                        direction -= 1
-                        col += 1
-                    if col > col_end:
-                        col = col_start
-                    if board[row][col] == '#':
-                        col -= 1
-                        if col < col_start:
-                            col = col_end
-                        break
-            elif facing == 'L':
-                col_start = 0
-                for i in range(len(board[row])):
-                    if board[row][i] != ' ':
-                        col_start = i
-                        break
-                col_end = len(board[row]) - 1
-                while direction > 0:
-                    if board[row][col] == '.':
-                        direction -= 1
-                        col -= 1
-                    if col < col_start:
-                        col = col_end
-                    if board[row][col] == '#':
-                        col += 1
-                        if col > col_end:
-                            col = col_start
-                        break
-            elif facing == 'U':
-                row_start = 0
-                for i in range(len(board)):
-                    if len(board[i]) - 1 >= col and board[i][col] != ' ':
-                        row_start = i
-                        break
-                row_end = 0
-                for i in range(row_start, len(board)):
-                    if len(board[i]) - 1 >= col and board[i][col] != ' ':
-                        row_end = i
-                while direction > 0:
-                    if board[row][col] == '.':
-                        direction -= 1
-                        row -= 1
-                    if row < row_start:
-                        row = row_end
-                    if board[row][col] == '#':
-                        row += 1
-                        if row > row_end:
-                            row = row_start
-                        break
-            elif facing == 'D':
-                row_start = 0
-                for i in range(len(board)):
-                    if len(board[i]) - 1 >= col and board[i][col] != ' ':
-                        row_start = i
-                        break
-                row_end = 0
-                for i in range(row_start, len(board)):
-                    if len(board[i]) - 1 >= col and board[i][col] != ' ':
-                        row_end = i
-                while direction > 0:
-                    if board[row][col] == '.':
-                        direction -= 1
-                        row += 1
-                    if row > row_end:
-                        row = row_start
-                    if board[row][col] == '#':
-                        row -= 1
-                        if row < row_start:
-                            row = row_end
-                        break
+        if isinstance(direction, int):
+            row, col = move(direction, facing, row, col, board)
+        else:
+            facing = direction_facing[direction][facing]
     row = row + 1
     col = col + 1
     face_value = {'R': 0, 'D': 1, 'L': 2, 'U': 3}
